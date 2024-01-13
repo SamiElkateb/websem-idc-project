10ac381ed4e42c7a7e7c0341ac39ff24719e4c44
Sami EL KATEB <67482346+SamiElkateb@users.noreply.github.com>
2024-01-13 22:35:40 +0100

    fix: recipe search query

--- kitchen_chef_server/kitchen_chef_server/sparql/queries/build_search_query.py
+++ kitchen_chef_server/kitchen_chef_server/sparql/queries/build_search_query.py
@@ -1,7 +1,8 @@
 def build_search_query(q_ingredients, q_filters):
     ingredients_query = ""
-    for i, _ in enumerate(q_ingredients):
-        ingredients_query += f"?recipe :hasIngredient ?ingredient{i} . ?ingredient{i} :food ?ingredientFood{i} ."
+    if q_ingredients:
+        for i, _ in enumerate(q_ingredients):
+            ingredients_query += f"?recipe :hasIngredient ?ingredient{i} . ?ingredient{i} :food ?ingredientFood{i} ."
     query = f"""
     prefix :<http://project-kitchenchef.fr/schema#>
     SELECT DISTINCT ?recipe ?name ?category ?instructions
@@ -21,6 +22,6 @@ def build_search_query(q_ingredients, q_filters):
                     :unit ?ingredientUnit .
 
         {ingredients_query}
-    }}
+    }} GROUP BY ?recipe
     """
     return query
