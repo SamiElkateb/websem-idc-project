c78cacd43233b63c0566de9dde31b75732014c3d
Sami EL KATEB <67482346+SamiElkateb@users.noreply.github.com>
2024-01-22 18:07:23 +0100

    fix: undefined recipe

--- kitchen_chef_app/src/pages/RecipesListPage.tsx
+++ kitchen_chef_app/src/pages/RecipesListPage.tsx
@@ -6,11 +6,11 @@ import RecipeCard from '../components/RecipeCard';
 import { TRecipe } from '../models/recipes';
 import { getRecipes } from '../api/recipes';
 
-const RecipeListPage:React.FC = () => {
+const RecipeListPage: React.FC = () => {
   const [recipes, setRecipes] = useState<TRecipe[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const handleUpdateRecipes = (newRecipes:TRecipe[]) => {
+  const handleUpdateRecipes = (newRecipes: TRecipe[]) => {
     setRecipes(newRecipes);
     setIsLoading(false);
   };
@@ -25,7 +25,7 @@ const RecipeListPage:React.FC = () => {
     <>
       <RecipeSearchForm onUpdateRecipes={handleUpdateRecipes} />
       <Grid container spacing={2} paddingY={10}>
-        {isLoading ? <Box margin="auto"><CircularProgress /></Box> : recipes.map((recipe) => (
+        {isLoading ? <Box margin="auto"><CircularProgress /></Box> : recipes.filter((recipe) => recipe.id || recipe.name).map((recipe) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={recipe.id}>
             <RecipeCard
               title={recipe?.name}
